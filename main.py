f8332f2c26873d6cbfa25166e751be95d210c6cf
Harry <162498817+1deals@users.noreply.github.com>
2024-11-03 23:00:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ async def ping(ctx):
   return await ctx.reply(f"latency: {round(bot.latency * 1000)}")
   
 # Run the bot using your token
-bot.run('MTI4OTE4ODc5MjA2NTk4MjQ2NA.GacT1d.3PrsiMWUOXtIwgr0WndUnaAFltJx7O1NcNuJWw')
+bot.run('MTIzMjc3ODUwODQyNjgwOTM2NQ.GdVCXH.s-fl_dnEm1YYAfDF0RpU87zNTQA2h5jnFpi8dg')
