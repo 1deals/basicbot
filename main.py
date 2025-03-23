b56bab4c35d6967f0fef8dbce70e612b99158468
Harry <162498817+1deals@users.noreply.github.com>
2025-03-23 21:17:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import jishaku
 
 intents = discord.Intents.all()
 
@@ -11,6 +12,7 @@ bot = commands.Bot(command_prefix="!", intents = intents)
 async def on_ready():
     print(f'Logged in as {bot.user}!')
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Be back soon..."))
+    await bot.load_extension("jishaku")
 
 # Command: !hello
 @bot.command()
@@ -18,4 +20,4 @@ async def ping(ctx):
   return await ctx.reply(f"latency: {round(bot.latency * 1000)}")
   
 # Run the bot using your token
-bot.run('MTIzMjc3ODUwODQyNjgwOTM2NQ.GdVCXH.s-fl_dnEm1YYAfDF0RpU87zNTQA2h5jnFpi8dg')
+bot.run('MTM1MzQ3NzM2MzAzOTczMTg2Mw.G_j77X.EMpop7Z9IbxEZjqAFpyjPPuN7AdJblaOUlFRks')
